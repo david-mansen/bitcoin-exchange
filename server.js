c50a7434560580a7de7c477abc38885959151e86
david-mansen <davidmansen@gmail.com>
2017-07-07 09:52:56 -0400

    Database schema and project skeleton

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require('./controllers/controller.js');
+
+app.use("/", routes);
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT " + PORT);
+});
+
+
